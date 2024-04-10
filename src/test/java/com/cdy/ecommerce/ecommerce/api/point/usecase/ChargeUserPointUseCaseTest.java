f6fe8e957a6c039a4060ae060249768c7a846e44
kabalport <kabalport@gmail.com>
2024-04-10 12:07:35 +0900

    포인트 조회api

--- src/test/java/com/cdy/ecommerce/ecommerce/api/point/usecase/ChargeUserPointUseCaseTest.java
+++ src/test/java/com/cdy/ecommerce/ecommerce/api/point/usecase/ChargeUserPointUseCaseTest.java
@@ -1,6 +1,9 @@
 package com.cdy.ecommerce.ecommerce.api.point.usecase;
 
 
+import com.cdy.ecommerce.ecommerce.domain.point.business.components.UserPointCharger;
+import com.cdy.ecommerce.ecommerce.domain.point.business.components.UserPointReader;
+
 class ChargeUserPointUseCaseTest {
-    
+
 }
